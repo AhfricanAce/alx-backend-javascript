e3c8aeec51492f2d858853f99e6867d9d6a9a0c5
Agbai Victor <ahfricanace@gmail.com>
2024-10-08 19:38:55 -0700

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Agbai Victor <https://github.com/AhfricanAce>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
